4029c41d655fb6446428e665403c5d0827a44d8a
Raghav Subbaraman <rsubbaraman@eng.ucsd.edu>
2021-02-05 15:21:28 -0800

    minor changes to lab2

--- grc/lab2/lab2_sim.py
+++ grc/lab2/lab2_sim.py
@@ -39,12 +39,11 @@ from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
 from gnuradio.qtgui import Range, RangeWidget
 from math import pi
-import iio
 import numpy as np
 
 from gnuradio import qtgui
 
-class lab2(gr.top_block, Qt.QWidget):
+class lab2_sim(gr.top_block, Qt.QWidget):
 
     def __init__(self):
         gr.top_block.__init__(self, "Lab 2")
@@ -67,7 +66,7 @@ class lab2(gr.top_block, Qt.QWidget):
         self.top_grid_layout = Qt.QGridLayout()
         self.top_layout.addLayout(self.top_grid_layout)
 
-        self.settings = Qt.QSettings("GNU Radio", "lab2")
+        self.settings = Qt.QSettings("GNU Radio", "lab2_sim")
 
         try:
             if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
@@ -84,7 +83,7 @@ class lab2(gr.top_block, Qt.QWidget):
         self.roll_off = roll_off = 0.5
         self.freqc = freqc = 900
         self.std_dev = std_dev = 0.05
-        self.samp_rate = samp_rate = 1024
+        self.samp_rate = samp_rate = 1000
         self.rrc_filter = rrc_filter = firdes.root_raised_cosine(4, sps, 1, roll_off, 32*sps+1)
         self.phase = phase = 0
         self.lw = lw = 2
@@ -139,13 +138,6 @@ class lab2(gr.top_block, Qt.QWidget):
         for r in range(10, 11):
             self.top_grid_layout.setRowStretch(r, 1)
         for c in range(1, 2):
-            self.top_grid_layout.setColumnStretch(c, 1)
-        self._freqc__range = Range(70, 6000, .01, freqc, 200)
-        self._freqc__win = RangeWidget(self._freqc__range, self.set_freqc_, 'Carrier (MHz)', "counter_slider", float)
-        self.top_grid_layout.addWidget(self._freqc__win, 10, 0, 1, 1)
-        for r in range(10, 11):
-            self.top_grid_layout.setRowStretch(r, 1)
-        for c in range(0, 1):
             self.top_grid_layout.setColumnStretch(c, 1)
         # Create the options list
         self._bNoise_options = (0, 1, )
@@ -318,7 +310,7 @@ class lab2(gr.top_block, Qt.QWidget):
             self.tab0_grid_layout_0.setColumnStretch(c, 1)
         self.qtgui_freq_sink_x_0_0 = qtgui.freq_sink_c(
             1024, #size
-            firdes.WIN_BLACKMAN_hARRIS, #wintype
+            firdes.WIN_RECTANGULAR, #wintype
             0, #fc
             samp_rate*1e3, #bw
             "", #name
@@ -458,9 +450,15 @@ class lab2(gr.top_block, Qt.QWidget):
         self.interp_fir_filter_xxx_1.declare_sample_delay(0)
         self.interp_fir_filter_xxx_0 = filter.interp_fir_filter_ccc(1, rrc_filter)
         self.interp_fir_filter_xxx_0.declare_sample_delay(0)
-        self.iio_pluto_source_0 = iio.pluto_source('', int(freqc_*1e6), int(samp_rate*1000), 20000000, buff_size, True, True, True, 'manual', 32, '', True)
-        self.iio_pluto_sink_0 = iio.pluto_sink('', int(freqc_*1e6), int(samp_rate*1000), 20000000, buff_size, False, 10.0, '', True)
+        self._freqc__range = Range(70, 6000, .01, freqc, 200)
+        self._freqc__win = RangeWidget(self._freqc__range, self.set_freqc_, 'Carrier (MHz)', "counter_slider", float)
+        self.top_grid_layout.addWidget(self._freqc__win, 10, 0, 1, 1)
+        for r in range(10, 11):
+            self.top_grid_layout.setRowStretch(r, 1)
+        for c in range(0, 1):
+            self.top_grid_layout.setColumnStretch(c, 1)
         self.digital_chunks_to_symbols_xx_1 = digital.chunks_to_symbols_bc(const.points(), 1)
+        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_gr_complex*1, samp_rate*1000,True)
         self.blocks_tag_gate_0 = blocks.tag_gate(gr.sizeof_gr_complex * 1, False)
         self.blocks_tag_gate_0.set_single_key("")
         self.blocks_selector_0_0 = blocks.selector(gr.sizeof_gr_complex*1,bFilter,0)
@@ -487,7 +485,7 @@ class lab2(gr.top_block, Qt.QWidget):
         self.blocks_complex_to_imag_0 = blocks.complex_to_imag(1)
         self.blocks_add_xx_0 = blocks.add_vcc(1)
         self.analog_random_source_x_0 = blocks.vector_source_b(list(map(int, numpy.random.randint(0, 2, 8192))), True)
-        self.analog_noise_source_x_0 = analog.noise_source_c(analog.GR_GAUSSIAN, std_dev, 0)
+        self.analog_noise_source_x_0 = analog.noise_source_c(analog.GR_GAUSSIAN, std_dev*(1.414), 0)
         self.analog_agc_xx_0 = analog.agc_cc(1e-4, 1.0, 1.0)
         self.analog_agc_xx_0.set_max_gain(65536)
 
@@ -506,14 +504,14 @@ class lab2(gr.top_block, Qt.QWidget):
         self.connect((self.blocks_complex_to_real_0, 0), (self.blocks_delay_0_2, 0))
         self.connect((self.blocks_complex_to_real_0, 0), (self.qtgui_freq_sink_x_0, 0))
         self.connect((self.blocks_complex_to_real_0_0, 0), (self.qtgui_histogram_sink_x_0, 0))
-        self.connect((self.blocks_deinterleave_0, 7), (self.blocks_selector_0, 7))
+        self.connect((self.blocks_deinterleave_0, 0), (self.blocks_selector_0, 0))
         self.connect((self.blocks_deinterleave_0, 4), (self.blocks_selector_0, 4))
         self.connect((self.blocks_deinterleave_0, 3), (self.blocks_selector_0, 3))
-        self.connect((self.blocks_deinterleave_0, 6), (self.blocks_selector_0, 6))
-        self.connect((self.blocks_deinterleave_0, 5), (self.blocks_selector_0, 5))
-        self.connect((self.blocks_deinterleave_0, 0), (self.blocks_selector_0, 0))
-        self.connect((self.blocks_deinterleave_0, 1), (self.blocks_selector_0, 1))
         self.connect((self.blocks_deinterleave_0, 2), (self.blocks_selector_0, 2))
+        self.connect((self.blocks_deinterleave_0, 7), (self.blocks_selector_0, 7))
+        self.connect((self.blocks_deinterleave_0, 6), (self.blocks_selector_0, 6))
+        self.connect((self.blocks_deinterleave_0, 1), (self.blocks_selector_0, 1))
+        self.connect((self.blocks_deinterleave_0, 5), (self.blocks_selector_0, 5))
         self.connect((self.blocks_delay_0, 0), (self.blocks_delay_0_0, 0))
         self.connect((self.blocks_delay_0, 0), (self.qtgui_time_sink_x_0, 1))
         self.connect((self.blocks_delay_0_0, 0), (self.blocks_delay_0_0_0, 0))
@@ -533,26 +531,26 @@ class lab2(gr.top_block, Qt.QWidget):
         self.connect((self.blocks_delay_0_1, 0), (self.qtgui_time_sink_x_0, 5))
         self.connect((self.blocks_delay_0_2, 0), (self.blocks_delay_0, 0))
         self.connect((self.blocks_delay_0_2, 0), (self.qtgui_time_sink_x_0, 0))
-        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.iio_pluto_sink_0, 0))
+        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.blocks_throttle_0, 0))
         self.connect((self.blocks_multiply_const_vxx_0_0, 0), (self.blocks_add_xx_0, 0))
         self.connect((self.blocks_multiply_const_vxx_0_0_0, 0), (self.blocks_selector_0_0, 0))
         self.connect((self.blocks_multiply_const_vxx_1, 0), (self.analog_agc_xx_0, 0))
         self.connect((self.blocks_selector_0, 0), (self.blocks_complex_to_real_0_0, 0))
-        self.connect((self.blocks_selector_0, 0), (self.qtgui_const_sink_x_0, 0))
         self.connect((self.blocks_selector_0, 0), (self.qtgui_const_sink_x_0, 1))
+        self.connect((self.blocks_selector_0, 0), (self.qtgui_const_sink_x_0, 0))
         self.connect((self.blocks_selector_0_0, 0), (self.blocks_multiply_const_vxx_0_0, 0))
         self.connect((self.blocks_tag_gate_0, 0), (self.blocks_complex_to_imag_0, 0))
         self.connect((self.blocks_tag_gate_0, 0), (self.blocks_complex_to_real_0, 0))
+        self.connect((self.blocks_throttle_0, 0), (self.blocks_multiply_const_vxx_1, 0))
         self.connect((self.digital_chunks_to_symbols_xx_1, 0), (self.interp_fir_filter_xxx_1, 0))
         self.connect((self.digital_chunks_to_symbols_xx_1, 0), (self.interp_fir_filter_xxx_1_0, 0))
-        self.connect((self.iio_pluto_source_0, 0), (self.blocks_multiply_const_vxx_1, 0))
         self.connect((self.interp_fir_filter_xxx_0, 0), (self.blocks_selector_0_0, 1))
         self.connect((self.interp_fir_filter_xxx_1, 0), (self.interp_fir_filter_xxx_0, 0))
         self.connect((self.interp_fir_filter_xxx_1_0, 0), (self.blocks_multiply_const_vxx_0_0_0, 0))
 
 
     def closeEvent(self, event):
-        self.settings = Qt.QSettings("GNU Radio", "lab2")
+        self.settings = Qt.QSettings("GNU Radio", "lab2_sim")
         self.settings.setValue("geometry", self.saveGeometry())
         event.accept()
 
@@ -591,15 +589,14 @@ class lab2(gr.top_block, Qt.QWidget):
 
     def set_std_dev(self, std_dev):
         self.std_dev = std_dev
-        self.analog_noise_source_x_0.set_amplitude(self.std_dev)
+        self.analog_noise_source_x_0.set_amplitude(self.std_dev*(1.414))
 
     def get_samp_rate(self):
         return self.samp_rate
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.iio_pluto_sink_0.set_params(int(self.freqc_*1e6), int(self.samp_rate*1000), 20000000, 10.0, '', True)
-        self.iio_pluto_source_0.set_params(int(self.freqc_*1e6), int(self.samp_rate*1000), 20000000, True, True, True, 'manual', 32, '', True)
+        self.blocks_throttle_0.set_sample_rate(self.samp_rate*1000)
         self.qtgui_freq_sink_x_0.set_frequency_range(0, self.samp_rate*1e3)
         self.qtgui_freq_sink_x_0_0.set_frequency_range(0, self.samp_rate*1e3)
 
@@ -636,8 +633,6 @@ class lab2(gr.top_block, Qt.QWidget):
 
     def set_freqc_(self, freqc_):
         self.freqc_ = freqc_
-        self.iio_pluto_sink_0.set_params(int(self.freqc_*1e6), int(self.samp_rate*1000), 20000000, 10.0, '', True)
-        self.iio_pluto_source_0.set_params(int(self.freqc_*1e6), int(self.samp_rate*1000), 20000000, True, True, True, 'manual', 32, '', True)
 
     def get_fps(self):
         return self.fps
@@ -703,7 +698,7 @@ class lab2(gr.top_block, Qt.QWidget):
 
 
 
-def main(top_block_cls=lab2, options=None):
+def main(top_block_cls=lab2_sim, options=None):
 
     if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
         style = gr.prefs().get_string('qtgui', 'style', 'raster')
